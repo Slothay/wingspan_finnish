8b64188a6d893561803f50e2fc0de3615103675e
Salla Kivistö <kivistosalla@gmail.com>
2025-01-31 17:59:32 +0200

    Minimum viable code ready for game night

--- main.py
+++ main.py
@@ -1,97 +1,63 @@
 from tkinter import *
+from tkinter import ttk
 import pandas
 from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
 LANG_FONT = ("Ariel", 40, "italic")
-WORD_FONT = ("Ariel", 10, "bold")
+WORD_FONT = ("Ariel", 20, "bold")
 
+def on_birdbox(click):
+    bird = click.widget.get()
+    bird_index = birds.index(bird)
+    description = finnish[bird_index]
+    card_frame.itemconfig(lang_text, text = bird)
+    card_frame.itemconfig(word_text, text = description)
 
+def on_bonusbox(click):
+    bonus = click.widget.get()
+    bonus_index = bonus_name.index(bonus)
+    description = finnish[bonus_index]
+    card_frame.itemconfig(lang_text, text = bonus)
+    card_frame.itemconfig(word_text, text = description)
+
+bird_names = pandas.read_csv('Wingspan_birds.csv', encoding='cp1252', sep=';')
+bonus_cards = pandas.read_csv('Wingspan_bonuscards.csv', encoding='cp1252', sep=';')
+birds = []
+finnish = []
+bonus_name = []
+bonus_finnish = []
+for i in range(len(bird_names)):
+    birds.append(bird_names.values[i][0])
+    finnish.append(bird_names.values[i][3])
+
+for i in range(len(bonus_cards)):
+    bonus_name.append(bonus_cards.values[i][0])
+    bonus_finnish.append(bonus_cards.values[i][2])
 
 #Create window
 window = Tk()
 window.title("Wingspan")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-card_frame = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_frame = Canvas(width=800, height=526, bg='white', highlightthickness=0)
 card_frame.grid(row=0, column=0, columnspan=2)
 
 lang_text = card_frame.create_text(400, 150, text="Bird - Bird in italic", fill="black", font=LANG_FONT)
-word_text = card_frame.create_text(400, 263, text="Nosta 3 bonuskorttia ja hylkää sitten 2. Voit hylätä bonuskortteja, joita et ole nostanut tällä vuorolla.", fill="black", font=WORD_FONT)
+word_text = card_frame.create_text(400, 263, text="Nosta 3 bonuskorttia ja hylkää sitten 2. Voit hylätä bonuskortteja, joita et ole nostanut tällä vuorolla.", width=800, fill="black", font=WORD_FONT)
 
-window.mainloop()
+bx = StringVar()
+birdbox = ttk.Combobox(window, width=50, textvariable=bx)
+birdbox['values'] = birds
+birdbox.grid(row=1, column=0)
 
+bo = StringVar()
+bonusbox = ttk.Combobox(window, width=50, textvariable=bx)
+bonusbox['values'] = bonus_name
+bonusbox.grid(row=1, column=1)
 
-
-'''
-try:
-    word_data = pandas.read_csv(".\\data\\words_to_learn.csv")
-except FileNotFoundError:
-    word_data = pandas.read_csv(".\\data\\kanji_data.csv")
-word_list_to_learn = word_data.to_dict(orient="records")
-flashcard = {}
-
-# ---------------------------- SAVE PROGRESS ------------------------------- #
-
-def word_known():
-    word_list_to_learn.remove(flashcard)
-
-    df = pandas.DataFrame(word_list_to_learn)
-    df.to_csv(".\\data\\words_to_learn.csv", index=False)
-    generate_word()
-
-
-# ---------------------------- FLIP THE CARD ------------------------------- #
-
-def flip_card():
-    card_frame.itemconfig(card_photo, image = card_back)
-    card_frame.itemconfig(lang_text, text="English", fill="white")
-    card_frame.itemconfig(word_text, text=flashcard["English"], fill="white")
-
-# ---------------------------- READ WORD FROM DATA ------------------------------- #
-
-def generate_word():
-    global flashcard, flip_timer
-    window.after_cancel(flip_timer)
-    flashcard = choice(word_list_to_learn)
-    card_frame.itemconfig(card_photo, image=card_front)
-    card_frame.itemconfig(lang_text, text=LANGUAGE, fill="black")
-    card_frame.itemconfig(word_text, text=flashcard[LANGUAGE], fill="black")
-    flip_timer = window.after(3000,flip_card)
-
-
-# ---------------------------- UI SETUP ------------------------------- #
-
-#Create window
-window = Tk()
-window.title("Flashcards")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-flip_timer = window.after(3000, func=flip_card)
-
-# Create Flashcard-photo
-card_frame = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-card_front = PhotoImage(file=".\\images\\card_front.png")
-card_back = PhotoImage(file=".\\images\\card_back.png")
-card_photo = card_frame.create_image(400, 263, image=card_front)
-card_frame.grid(row=0, column=0, columnspan=2)
-
-lang_text = card_frame.create_text(400, 150, text=LANGUAGE, fill="black", font=LANG_FONT)
-word_text = card_frame.create_text(400, 263, text="Word", fill="black", font=WORD_FONT)
-
-# Create correct-button
-correct_img = PhotoImage(file=".\\images\\right.png")
-correct_answer = Button(image=correct_img, highlightthickness=0, borderwidth=0, command=word_known)
-correct_answer.grid(row=1,column=0)
-
-# Create incorrect-button
-
-incorrect_img = PhotoImage(file=".\\images\\wrong.png")
-incorrect_answer = Button(image=incorrect_img, highlightthickness=0, borderwidth=0, command=generate_word)
-incorrect_answer.grid(row=1,column=1)
-
-generate_word()
+birdbox.bind('<<ComboboxSelected>>', on_birdbox)
+bonusbox.bind('<<ComboboxSelected>>', on_bonusbox)
 
 
 window.mainloop()
-'''
